e1fa5c4d3fa0cadd81e266ec2317a11abfc90722
arturmineev9 <arturmineev9@mail.ru>
2024-10-26 14:07:54 +0300

    Created navigation between first and second fragment

--- HW1_Fragments/app/src/main/java/com/example/hw1_fragments/fragments/SecondFragment.kt
+++ HW1_Fragments/app/src/main/java/com/example/hw1_fragments/fragments/SecondFragment.kt
@@ -6,55 +6,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.hw1_fragments.R
+import com.example.hw1_fragments.databinding.FragmentFirstBinding
+import com.example.hw1_fragments.databinding.FragmentSecondBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SecondFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SecondFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SecondFragment : Fragment(R.layout.fragment_second) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var viewBinding: FragmentSecondBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        viewBinding = FragmentSecondBinding.inflate(inflater, container, false)
+        val enteredText = arguments?.getString("arg_entered_text") ?: "Текст не передан"
+        viewBinding?.run {
+            textView.text = enteredText.toString()
+        }
+        return viewBinding?.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        private const val ARG_ENTERED_TEXT = "arg_entered_text"
+
+        // Метод для создания нового экземпляра SecondFragment с текстом
+        fun newInstance(enteredText: String) = SecondFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_ENTERED_TEXT, enteredText)
             }
+        }
     }
 }
