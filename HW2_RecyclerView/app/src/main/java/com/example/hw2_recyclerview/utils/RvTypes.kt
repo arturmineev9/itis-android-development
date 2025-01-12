b16eda85f3196e85699671bb208d8b4a27d01512
arturmineev9 <arturmineev9@mail.ru>
2025-01-12 18:24:50 +0300

    Удаление HW2 и HW3 из ветки main

--- HW2_RecyclerView/app/src/main/java/com/example/hw2_recyclerview/utils/RvTypes.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hw2_recyclerview.utils
-
-// Перечисление типов списков
-enum class RvTypes {
-    LIST,
-    GRID,
-    MODIFIED_GRID
-}
--- HW3_ViewPager/app/src/main/java/com/example/hw3_viewpager/QuestionModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hw3_viewpager
-
-// Модель вопроса
-data class QuestionModel(
-    val id: Int,
-    val question : String,
-    val answers: List<String>
-)
