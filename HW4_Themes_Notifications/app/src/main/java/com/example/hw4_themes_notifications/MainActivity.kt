ac32f3e6ca466dc669c8709d2169d7d3506dbdcb
arturmineev9 <arturmineev9@mail.ru>
2025-01-13 12:27:54 +0300

    Fixinf bugs

--- HW4_Themes_Notifications/app/src/main/java/com/example/hw4_themes_notifications/MainActivity.kt
+++ HW4_Themes_Notifications/app/src/main/java/com/example/hw4_themes_notifications/MainActivity.kt
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.hw4_themes_notifications.databinding.ActivityMainBinding
 
