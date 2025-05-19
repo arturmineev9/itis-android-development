deb5de3a365100bf4643389498f05f672c10a622
arturmineev9 <arturmineev9@mail.ru>
2025-05-20 00:28:29 +0300

    minor changes

--- HW3_Sem2_Recompose/feature/graph-screen/src/main/java/ru/itis/clientserverapp/graph_screen/viewmodel/GraphViewModel.kt
+++ HW3_Sem2_Recompose/feature/graph-screen/src/main/java/ru/itis/clientserverapp/graph_screen/viewmodel/GraphViewModel.kt
@@ -36,15 +36,38 @@ class GraphViewModel @Inject constructor(
 
     fun buildGraph() {
         val count = pointCountInput.value.toIntOrNull()
-        val inputValues = valuesInput.value.split(",").mapNotNull { it.trim().toFloatOrNull() }
+        val inputValues = valuesInput.value.split(",")
+            .map { it.trim() }
+            .filter { it.isNotBlank() }
+            .mapNotNull {
+                try {
+                    it.toFloat()
+                } catch (e: NumberFormatException) {
+                    null
+                }
+            }
 
         when {
-            count == null || count <= 0 ->
+            pointCountInput.value.isBlank() || valuesInput.value.isBlank() -> {
+                _error.value = context.getString(R.string.fields_cannot_be_empty)
+            }
+            count == null -> {
+                _error.value = context.getString(R.string.invalid_number_format)
+            }
+            count <= 0 -> {
                 _error.value = context.getString(R.string.wrong_points_number)
-            inputValues.size != count ->
+            }
+            inputValues.size != valuesInput.value.split(",")
+                .map { it.trim() }
+                .filter { it.isNotBlank() }.size -> {
+                _error.value = context.getString(R.string.invalid_values_format)
+            }
+            inputValues.size != count -> {
                 _error.value = context.getString(R.string.number_of_values_must_match)
-            inputValues.any { it < 0 } ->
+            }
+            inputValues.any { it < 0 } -> {
                 _error.value = context.getString(R.string.values_must_be_non_negative)
+            }
             else -> {
                 _error.value = null
                 _values.value = inputValues
